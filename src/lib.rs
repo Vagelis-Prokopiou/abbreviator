0f5c1958ffd96deab607508f66df4cf148317038
rdxdkr <franchi.davide95@gmail.com>
2023-01-30 21:45:38 +0100

    used optional char instead of string

--- src/lib.rs
+++ src/lib.rs
@@ -3,27 +3,30 @@ pub fn abbreviate(word: &str) -> String {
     if word_length < 3 {
         return word.to_string();
     }
+    /*
+        At this point of the code the following unwraps should always  
+        succeed because they can only return None if the word is empty.
+
+        If they fail they return the default value for char, which is '\x00':
+        https://doc.rust-lang.org/std/primitive.char.html#impl-Default-for-char
+    */
     format!(
         "{}{}{}",
-        get_first_letter(word),
+        get_first_letter(word).unwrap_or_default(),
         word_length - 2,
-        get_last_letter(word)
+        get_last_letter(word).unwrap_or_default()
     )
 }
 
-fn get_nth_letter(word: &str, index: usize) -> String {
-    if word.is_empty() {
-        return word.to_string();
-    }
-
-    return word.chars().nth(index).unwrap().to_string();
+fn get_nth_letter(word: &str, index: usize) -> Option<char> {
+    word.chars().nth(index)
 }
 
-fn get_first_letter(word: &str) -> String {
+fn get_first_letter(word: &str) -> Option<char> {
     get_nth_letter(word, 0)
 }
 
-fn get_last_letter(word: &str) -> String {
+fn get_last_letter(word: &str) -> Option<char> {
     let index = word.chars().count().saturating_sub(1);
     get_nth_letter(word, index)
 }
@@ -34,18 +37,18 @@ mod tests {
 
     #[test]
     fn test_get_first_letter() {
-        assert_eq!(get_first_letter("hello"), "h");
-        assert_eq!(get_first_letter("HELLO"), "H");
-        assert_eq!(get_first_letter("a"), "a");
-        assert_eq!(get_first_letter(""), "");
+        assert_eq!(get_first_letter("hello"), Some('h'));
+        assert_eq!(get_first_letter("HELLO"), Some('H'));
+        assert_eq!(get_first_letter("a"), Some('a'));
+        assert_eq!(get_first_letter(""), None);
     }
 
     #[test]
     fn test_get_last_letter() {
-        assert_eq!(get_last_letter("hello"), "o");
-        assert_eq!(get_last_letter("HELLO"), "O");
-        assert_eq!(get_last_letter("a"), "a");
-        assert_eq!(get_last_letter(""), "");
+        assert_eq!(get_last_letter("hello"), Some('o'));
+        assert_eq!(get_last_letter("HELLO"), Some('O'));
+        assert_eq!(get_last_letter("a"), Some('a'));
+        assert_eq!(get_last_letter(""), None);
     }
 
     #[test]
