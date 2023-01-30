8332129219243c8217f38bbd02160e5393dbd816
rdxdkr <franchi.davide95@gmail.com>
2023-01-30 22:42:01 +0100

    added handling of UTF-8 grapheme clusters

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,15 @@
+use unicode_segmentation::{Graphemes, UnicodeSegmentation};
+
 pub fn abbreviate(word: &str) -> String {
-    let word_length = word.chars().count();
+    let word_length = graphemes_count(word);
     if word_length < 3 {
         return word.to_string();
     }
     /*
-        At this point of the code the following unwraps should always  
+        At this point of the code the following unwraps should always
         succeed because they can only return None if the word is empty.
 
-        If they fail they return the default value for char, which is '\x00':
-        https://doc.rust-lang.org/std/primitive.char.html#impl-Default-for-char
+        If they fail they return the default empty string "".
     */
     format!(
         "{}{}{}",
@@ -18,17 +19,25 @@ pub fn abbreviate(word: &str) -> String {
     )
 }
 
-fn get_nth_letter(word: &str, index: usize) -> Option<char> {
-    word.chars().nth(index)
+fn get_nth_letter(word: &str, index: usize) -> Option<&str> {
+    word_graphemes(word).nth(index)
 }
 
-fn get_first_letter(word: &str) -> Option<char> {
+fn get_first_letter(word: &str) -> Option<&str> {
     get_nth_letter(word, 0)
 }
 
-fn get_last_letter(word: &str) -> Option<char> {
-    let index = word.chars().count().saturating_sub(1);
+fn get_last_letter(word: &str) -> Option<&str> {
+    let index = graphemes_count(word).saturating_sub(1);
     get_nth_letter(word, index)
+}
+
+fn word_graphemes(word: &str) -> Graphemes {
+    word.graphemes(true)
+}
+
+fn graphemes_count(word: &str) -> usize {
+    word_graphemes(word).count()
 }
 
 #[cfg(test)]
@@ -37,17 +46,17 @@ mod tests {
 
     #[test]
     fn test_get_first_letter() {
-        assert_eq!(get_first_letter("hello"), Some('h'));
-        assert_eq!(get_first_letter("HELLO"), Some('H'));
-        assert_eq!(get_first_letter("a"), Some('a'));
+        assert_eq!(get_first_letter("hello"), Some("h"));
+        assert_eq!(get_first_letter("HELLO"), Some("H"));
+        assert_eq!(get_first_letter("a"), Some("a"));
         assert_eq!(get_first_letter(""), None);
     }
 
     #[test]
     fn test_get_last_letter() {
-        assert_eq!(get_last_letter("hello"), Some('o'));
-        assert_eq!(get_last_letter("HELLO"), Some('O'));
-        assert_eq!(get_last_letter("a"), Some('a'));
+        assert_eq!(get_last_letter("hello"), Some("o"));
+        assert_eq!(get_last_letter("HELLO"), Some("O"));
+        assert_eq!(get_last_letter("a"), Some("a"));
         assert_eq!(get_last_letter(""), None);
     }
 
@@ -69,5 +78,21 @@ mod tests {
         assert_eq!(abbreviate("Άντε και στα δικά σου!"), "Ά20!");
         assert_eq!(abbreviate("σκουλικομερμυγκότρυπα"), "σ19α");
         assert_eq!(abbreviate("Άσπρη πέτρα ξέξασπρη κι απ' τον ήλιο ξεξασπρώτερη"), "Ά47η");
+
+        // Misc
+        // https://doc.rust-lang.org/book/ch08-02-strings.html#creating-a-new-string
+        // https://unicode-rs.github.io/unicode-segmentation/unicode_segmentation/trait.UnicodeSegmentation.html#tymethod.grapheme_indices
+        assert_eq!(abbreviate("عليكم"), "ع3م");
+        assert_eq!(abbreviate("السلام"), "ا4م");
+        assert_eq!(abbreviate("Dobrý"), "D3ý");
+        assert_eq!(abbreviate("שָׁלוֹם"), "שָׁ2ם");
+        assert_eq!(abbreviate("नमस्ते"), "न2ते");
+        assert_eq!(abbreviate("こんにちは"), "こ3は");
+        assert_eq!(abbreviate("안녕하세요"), "안3요");
+        assert_eq!(abbreviate("你好"), "你好");
+        assert_eq!(abbreviate("Olá"), "O1á");
+        assert_eq!(abbreviate("Здравствуйте"), "З10е");
+        assert_eq!(abbreviate("Hola"), "H2a");
+        assert_eq!(abbreviate("a̐éö̲"), "a̐1ö̲");
     }
 }
